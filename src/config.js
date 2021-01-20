98a772364fe1de45331fdebdddaafcaff02a0776
Andrew Coleburn <arcoleburn@gmail.com>
2021-01-20 16:25:58 -0500

    init set up

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  API_ENDPOINT: `https://sleepy-harbor-21485.herokuapp.com/api`,
+  // API_ENDPOINT: `https://sleepy-harbor-21485.herokuapp.com/api`,
+  API_ENDPOINT: 'http://localhost:8000/api'
 };
