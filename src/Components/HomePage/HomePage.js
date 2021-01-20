ac2b8377d74ecbada8ad2ef80e39965cb89d4fdd
Andrew Coleburn <arcoleburn@gmail.com>
2021-01-20 16:25:27 -0500

    home page edits

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,24 +1,23 @@
-import React from "react";
-import Context from "../Context/Context";
+import React from 'react';
+import Context from '../Context/Context';
 
 export default function HomePage(props) {
   const handleClick = (e, context) => {
     e.preventDefault();
-    console.log("handle click on home page was clicked");
     const name = e.target.name.value;
     console.log(name);
     const nameData = {
       name: name,
     };
     if (!name) {
-      context.setError("Please Enter Valid Name");
+      context.setError('Please enter your name');
       return;
     } else {
       context.setName(name);
       context.addPeople(nameData);
-      context.setError("");
+      context.setError('');
       console.log(context.name);
-      props.history.push("/confirmation");
+      props.history.push('/confirmation');
     }
   };
   return (
@@ -27,12 +26,16 @@ export default function HomePage(props) {
         return (
           <div className="home">
             <div className="homeSection">
-              <h1>Welcome To Petful</h1>
+              <h1>Welcome To F.I.F.O FIDO</h1>
               <p>
-                We are an animal shelter for cats and dogs. Adoption is on a
-                first come first serve basis. If you are lucky, you may be first
-                in line! If not, you will be put in a queue before you can take
-                your new pet home.
+                We are an animal shelter dogs and cats too! We do
+                things a little differently here. Adoption is on a
+                first come first serve basis. If you are lucky, you
+                may be first in line! If not, you will be put in a
+                queue before you can take your new pet home.
+                Additionally, the only pets up for adoption at any
+                given time are the dog and cat who have been in our
+                shelter the longest. First In, First Out! FIFO FIDO!
               </p>
               <form
                 onSubmit={(e) => {
@@ -40,13 +43,17 @@ export default function HomePage(props) {
                 }}
               >
                 <p>
-                  <label>Enter your name here: </label>
+                  <label>What's your name?</label>
                 </p>
                 <p>
-                  <input name="name" type="text" placeholder="John Smith" />
+                  <input
+                    name="name"
+                    type="text"
+                    placeholder="John Smith"
+                  />
                 </p>
                 <p className="error">{context.error}</p>
-                <button type="submit">Start Your Search Now!</button>
+                <button type="submit">Find your furrever friend!</button>
               </form>
             </div>
           </div>
