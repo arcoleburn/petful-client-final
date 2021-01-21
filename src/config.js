ec196707fa95546af6cf4e13d39211ddd766860a
Andrew Coleburn <arcoleburn@gmail.com>
2021-01-21 11:31:22 -0500

    issue with api link

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  API_ENDPOINT: `http://petful-server.herokuapp.com/api`,
+  API_ENDPOINT: `https://petful-server.herokuapp.com/api`,
   // API_ENDPOINT: 'http://localhost:8000/api'
 };
